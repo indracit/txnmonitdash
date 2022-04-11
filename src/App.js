b59289fbf7448052c1b01935ecc784ecfa36ad85
Indracit S <indrajithij802@gmail.com>
2022-04-11 12:04:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+import axios from 'axios'
+import './table.css'
+import { Button } from '@mui/material';
+import { Box } from '@mui/material';
+
+const heading = ["SerialNo","RRN","GatewayRRN","VendorID","RespToClient","Amount","ServiceName","ONUS","ServerTime"]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+  const [value,setValue]=useState([])
+async function fetchData() {
+const response = await axios.get("url");
+return response.data
+}
+
+return ( <div  style={{fontSize:'.8em'}}>  <h1 style={{"text-align": "center"}}>DASHBOARD</h1>
+                   <Box textAlign='center' m={2}> <Button variant="outlined"  onClick={()=>{fetchData().then(data => {
+  console.log('data fetched from misdashboard');
+setValue(data) 
+});}}>Get Data</Button>    </Box>   
+<div style={{ display:'flex',"justify-content": "center", height: '70vh', maxWidth: '100vw', }}>
+<table style={{ width: '85vw' }}>
+                <thead>
+                    <tr>
+                        {heading.map(head => <th>{head}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {value.map(row =>{
+                      return  <tr> {row.map(val=> <td>{val}</td>)}
+                              </tr>})}
+                </tbody>
+            </table>
+</div> </div>
   );
 }
 
